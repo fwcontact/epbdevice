3a5642d54eed50cd1b5331f6af5d627e0bdc4379
Frank Wang <fwcontact@me.com>
2020-07-02 19:51:26 +0800

    1.4.1, convert using jackson-databind

--- src/main/java/com/epb/epbdevice/beans/PrintPool.java
+++ src/main/java/com/epb/epbdevice/beans/PrintPool.java
@@ -8,146 +8,151 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 
-/**
- *
- */
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class PrintPool implements Serializable {
 
-    private BigDecimal lineNo;
-    private BigInteger orderNo;
-    private String printCommand;
-    private String const1;
-    private String const2;
-    private String format;
-    private BigInteger length;
-    private String align;
-    private String breakFlg;
-    private String fillBlankFlg;
-    private String val;
-    private String printPort;
-    private BigInteger printSeq;
-    
-    private static final String EMPTY = "";
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4768110116317408705L;
 
-    public PrintPool() {
-        super();
-        this.lineNo = null;
-        this.orderNo = null;
-        this.printCommand = EMPTY;
-        this.const1 = EMPTY;
-        this.const2 = EMPTY;
-        this.format = EMPTY;
-        this.length = null;
-        this.align = EMPTY;
-        this.breakFlg = EMPTY;
-        this.fillBlankFlg = EMPTY;
-        this.val = EMPTY;
-        this.printPort = EMPTY;
-        this.printSeq = null;
-    }
+	private BigDecimal lineNo;
+	private BigInteger orderNo;
+	private String printCommand;
+	private String const1;
+	private String const2;
+	private String format;
+	private BigInteger length;
+	private String align;
+	private String breakFlg;
+	private String fillBlankFlg;
+	private String val;
+	private String printPort;
+	private BigInteger printSeq;
 
-    public BigDecimal getLineNo() {
-        return lineNo;
-    }
+	private static final String EMPTY = "";
 
-    public void setLineNo(BigDecimal lineNo) {
-        this.lineNo = lineNo;
-    }
+	public PrintPool() {
+		super();
+		this.lineNo = null;
+		this.orderNo = null;
+		this.printCommand = EMPTY;
+		this.const1 = EMPTY;
+		this.const2 = EMPTY;
+		this.format = EMPTY;
+		this.length = null;
+		this.align = EMPTY;
+		this.breakFlg = EMPTY;
+		this.fillBlankFlg = EMPTY;
+		this.val = EMPTY;
+		this.printPort = EMPTY;
+		this.printSeq = null;
+	}
 
-    public BigInteger getOrderNo() {
-        return orderNo;
-    }
+	public BigDecimal getLineNo() {
+		return lineNo;
+	}
 
-    public void setOrderNo(BigInteger orderNo) {
-        this.orderNo = orderNo;
-    }
+	public void setLineNo(BigDecimal lineNo) {
+		this.lineNo = lineNo;
+	}
 
-    public String getPrintCommand() {
-        return printCommand;
-    }
+	public BigInteger getOrderNo() {
+		return orderNo;
+	}
 
-    public void setPrintCommand(String printCommand) {
-        this.printCommand = printCommand;
-    }
+	public void setOrderNo(BigInteger orderNo) {
+		this.orderNo = orderNo;
+	}
 
-    public String getConst1() {
-        return const1;
-    }
+	public String getPrintCommand() {
+		return printCommand;
+	}
 
-    public void setConst1(String const1) {
-        this.const1 = const1;
-    }
+	public void setPrintCommand(String printCommand) {
+		this.printCommand = printCommand;
+	}
 
-    public String getConst2() {
-        return const2;
-    }
+	public String getConst1() {
+		return const1;
+	}
 
-    public void setConst2(String const2) {
-        this.const2 = const2;
-    }
+	public void setConst1(String const1) {
+		this.const1 = const1;
+	}
 
-    public String getFormat() {
-        return format;
-    }
+	public String getConst2() {
+		return const2;
+	}
 
-    public void setFormat(String format) {
-        this.format = format;
-    }
+	public void setConst2(String const2) {
+		this.const2 = const2;
+	}
 
-    public BigInteger getLength() {
-        return length;
-    }
+	public String getFormat() {
+		return format;
+	}
 
-    public void setLength(BigInteger length) {
-        this.length = length;
-    }
+	public void setFormat(String format) {
+		this.format = format;
+	}
 
-    public String getAlign() {
-        return align;
-    }
+	public BigInteger getLength() {
+		return length;
+	}
 
-    public void setAlign(String align) {
-        this.align = align;
-    }
+	public void setLength(BigInteger length) {
+		this.length = length;
+	}
 
-    public String getBreakFlg() {
-        return breakFlg;
-    }
+	public String getAlign() {
+		return align;
+	}
 
-    public void setBreakFlg(String breakFlg) {
-        this.breakFlg = breakFlg;
-    }
+	public void setAlign(String align) {
+		this.align = align;
+	}
 
-    public String getFillBlankFlg() {
-        return fillBlankFlg;
-    }
+	public String getBreakFlg() {
+		return breakFlg;
+	}
 
-    public void setFillBlankFlg(String fillBlankFlg) {
-        this.fillBlankFlg = fillBlankFlg;
-    }
+	public void setBreakFlg(String breakFlg) {
+		this.breakFlg = breakFlg;
+	}
 
-    public String getVal() {
-        return val;
-    }
+	public String getFillBlankFlg() {
+		return fillBlankFlg;
+	}
 
-    public void setVal(String val) {
-        this.val = val;
-    }
+	public void setFillBlankFlg(String fillBlankFlg) {
+		this.fillBlankFlg = fillBlankFlg;
+	}
 
-    public String getPrintPort() {
-        return printPort;
-    }
+	public String getVal() {
+		return val;
+	}
 
-    public void setPrintPort(String printPort) {
-        this.printPort = printPort;
-    }
+	public void setVal(String val) {
+		this.val = val;
+	}
 
-    public BigInteger getPrintSeq() {
-        return printSeq;
-    }
+	public String getPrintPort() {
+		return printPort;
+	}
 
-    public void setPrintSeq(BigInteger printSeq) {
-        this.printSeq = printSeq;
-    }
-    
+	public void setPrintPort(String printPort) {
+		this.printPort = printPort;
+	}
+
+	public BigInteger getPrintSeq() {
+		return printSeq;
+	}
+
+	public void setPrintSeq(BigInteger printSeq) {
+		this.printSeq = printSeq;
+	}
+
 }
